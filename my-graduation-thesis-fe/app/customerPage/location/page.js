d0c12a3031e6fc7af8a764d1380c0584be17429f
TienLee2 <nguyenletien21052002@gmail.com>
2024-01-19 16:40:29 +0700

    UserLocationMap

--- my-graduation-thesis-fe/app/customerPage/location/page.js
+++ my-graduation-thesis-fe/app/customerPage/location/page.js
@@ -1,72 +1,17 @@
 "use client";
-import React, { useEffect } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css"; // Import the Leaflet CSS
-import "leaflet-routing-machine";
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('../../../components/Map'), { ssr: false });
 
 const LocationPage = () => {
-  useEffect(() => {
-    const mapContainer = document.getElementById("map");
-
-    if (mapContainer && !mapContainer._leaflet_id) {
-      // Container exists and map is not initialized yet
-      const map = L.map("map").setView([10.027008, 105.74889], 13);
-
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "© OpenStreetMap contributors",
-      }).addTo(map);
-
-      var marker = L.marker([10.027008, 105.74889])
-        .addTo(map)
-        .bindPopup("Hello")
-        .openPopup();
-
-      var popup = L.popup();
-
-      var routingControl; // Variable to store the reference to the routing control
-
-      function onMapClick(e) {
-        // Close existing popup if any
-        map.closePopup();
-        // Remove the existing routing control if any
-        if (routingControl) {
-          map.removeControl(routingControl);
-        }
-
-        // Create a new popup and open it on the map
-        popup
-          .setLatLng(e.latlng)
-          .setContent("You clicked the map at " + e.latlng.toString())
-          .openOn(map);
-
-        // Create a new marker at the clicked location
-        var endMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-
-        // Create a new routing control with waypoints
-        routingControl = L.Routing.control({
-          waypoints: [
-            L.latLng([10.027008, 105.74889]),
-            L.latLng(e.latlng.lat, e.latlng.lng),
-          ],
-        }).addTo(map);
-
-        // Update the waypoints in the existing control
-        routingControl.spliceWaypoints(
-          routingControl.getWaypoints().length - 1,
-          1,
-          e.latlng
-        );
-      }
-
-      map.on("click", onMapClick);
-    }
-  }, []);
+ 
 
   return (
-    <div>
-      <div id="map" style={{ height: "800px" }}></div>
-      <div id="root"></div>
-    </div>
+    <>
+      <div>
+          <Map/>
+      </div>
+    </>
   );
 };
 
