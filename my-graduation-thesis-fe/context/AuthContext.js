810cee43183f8cd934b7e9ba07ea00964907308e
MinhHien <leminhhien.bl@gmail.com>
2024-02-13 22:52:38 +0700

    Fix Product Detaill & Edit

--- my-graduation-thesis-fe/context/AuthContext.js
+++ my-graduation-thesis-fe/context/AuthContext.js
@@ -226,6 +226,16 @@ export const AuthProvider = ({ children }) => {
           let token = data.resultObj;
           Cookies.set("token", token);
           Cookies.set("userId", userId);
+          function parseJwt(token) {
+            if (!token) {
+              return;
+            }
+            const base64Url = token.split(".")[1];
+            const base64 = base64Url.replace("-", "+").replace("_", "/");
+            return JSON.parse(window.atob(base64));
+          }
+
+          console.log("Tokken ne: " + JSON.stringify(parseJwt(token)));
 
           // Success logic
           Notification.close(idsTmp.shift());
