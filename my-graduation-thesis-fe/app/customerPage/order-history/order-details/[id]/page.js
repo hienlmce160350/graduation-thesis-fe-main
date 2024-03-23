3306daa7cde96d08cfd6ea884be33d0aeae1d2b8
MinhHien <leminhhien.bl@gmail.com>
2024-03-24 00:33:35 +0700

    commit-2

--- my-graduation-thesis-fe/app/customerPage/order-history/order-details/[id]/page.js
+++ my-graduation-thesis-fe/app/customerPage/order-history/order-details/[id]/page.js
@@ -259,7 +259,7 @@ const OrderDetail = () => {
 
   return (
     <>
-      <div className="max-w-7xl mx-auto my-4 px-4 min-h-[100vh]">
+      <div className="max-w-7xl mx-auto my-4 px-4">
         <div className="p-[7px] bg-[#eee]">
           <Breadcrumb compact={false}>
             <Breadcrumb.Item icon={<IconHome />}>
